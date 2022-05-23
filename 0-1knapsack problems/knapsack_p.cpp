bf6c102468448d2ed58024bb2cf71104aad32ef2
Neeraj singh Bhandari <92501172+Neeraj-Bhandari44@users.noreply.github.com>
2022-05-23 15:40:14 +0530

    Update knapsack_p.cpp

--- 0-1knapsack problems/knapsack_p.cpp
+++ 0-1knapsack problems/knapsack_p.cpp
@@ -1,28 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-//recursively
-// int knapsack(int wt[],int val[],int w,int n)
-// {
-//     int static t[102][1002];
-// memset(t,-1,sizeof(t));
-//     if(n==0||w==0)
-//     return 0;
-//     if(t[n][w]!=-1)
-//     return t[n][w];
-//     if(wt[n-1]<=w)
-//     return t[n][w]=max(val[n-1]+knapsack(wt,val,w-wt[n-1],n-1),knapsack(wt,val,w,n-1));
-//     else if (wt[n-1]>w)
-//     return t[n][w]=knapsack(wt,val,w,n-1);
-// }
-//iteratively
 int knapsack(int wt[],int val[],int w,int n)
 {
     int static t[102][1002];
     for(int i=0;i<n+1;i++)
     {
-        for(int j=0;j<w+1;j++)
+        for(int j=0;j<w+1;j++){
         if(i==0||j==0)
         t[i][j]=0;
+       }  
+    }
         for(int i=0;i<n+1;i++)
         {
             for(int j=0;j<w+1;j++)
@@ -34,7 +21,7 @@ int knapsack(int wt[],int val[],int w,int n)
                 }
             }
         }
-    }
+    return t[n][w];
 }
 int main()
 {
